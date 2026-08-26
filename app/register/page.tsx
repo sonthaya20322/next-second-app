7ebe186ca3f1ba057d02a8bada0847f2d5b8ea0c
sonthaya20322 <s6752410004@sau.ac.th>
2026-08-26 14:35:17 +0700

    วิธีการอ้างอิงรูปภาพ

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,46 @@
+import Image from "next/image";
+import fire from "./../assets/images/fire.png";
+import snowy from "./../assets/images/snowy.png";
+
 export default function Page() {
     return (
         <div>
-            <h1>
+            <h1 className="text-green-800 text-4xl text-center border-2 border-green-800 rounded-lg p-4">
                 Register
             </h1>
+            <hr />
+            {/* {อ้างอิงรูปภาพ public} */}
+            <Image
+                src="/earth.png" 
+                alt="register" 
+                width={120} 
+                height={120}
+                className="rounded-full border-2 border-green-800 rounded-2xl p-4 mx-auto mt-1"
+            />
+            {/* {อ้างอิงรูปภาพ assets/images} */}
+            <Image
+                src={snowy}
+                alt="register"
+                width={120}
+                height={120}
+                className="rounded-full border-2 border-green-800 rounded-2xl p-4 mx-auto mt-1"
+            />
+            <Image
+                src={fire}
+                alt="register"
+                width={120}
+                height={120}
+                className="rounded-full border-2 border-green-800 rounded-2xl p-4 mx-auto mt-1"
+            />
+            {/* {อ้างอิงรูปภาพ network or internet} */}
+            <Image
+                src="https://images.pexels.com/photos/38897361/pexels-photo-38897361.jpeg"
+                alt="register"
+                width={120}
+                height={120}
+                className="rounded-full border-2 border-green-800 rounded-2xl p-4 mx-auto mt-1"
+            />
+            
         </div>
     );
 }
