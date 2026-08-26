65936dc62329d02aee16d535bade984b7237f0f0
sonthaya20322 <s6752410004@sau.ac.th>
2026-08-26 11:48:29 +0700

    <br />

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,23 @@ export default function Page() {
     <div>
       <h1>ID : 6752410004</h1>
       <h1>Name : Shonthaya Saiwanna</h1>
+      <br />
       <hr />
-      <h3>Link to Basic Route Page</h3>
-      <Link href="/register">[Register]</Link>
-      <Link href="/products">[Products]</Link>
-      <Link href="/member">[Member]</Link> 
+      <br />
+      <h3>Link to Basic Route Page</h3> <br />
+      <Link href="/register">[Register]</Link> <br />
+      <Link href="/products">[Products]</Link> <br />
+      <Link href="/member">[Member]</Link> <br />
 
 
-
+      <br />
       <hr />
-      <h3>Link to Basic Route Page</h3>
-      <Link href="/products/keybord">[Keyboard]</Link>
-      <Link href="/products/mouse">[Mouse]</Link>
-      <Link href="/products/notebook/acer">[Notebook Acer]</Link>
-      <Link href="/products/notebook/rog">[Notebook Rog]</Link>
+      <br />
+      <h3>Link to Basic Route Page</h3> <br />
+      <Link href="/products/keybord">[Keyboard]</Link> <br />
+      <Link href="/products/mouse">[Mouse]</Link> <br />
+      <Link href="/products/notebook/acer">[Notebook Acer]</Link> <br />
+      <Link href="/products/notebook/rog">[Notebook Rog]</Link> <br />
 
 
 
